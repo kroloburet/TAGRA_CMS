a77fa28f2bdbe6074701b3dac25f7c9333e78afa
kroloburet <kroloburet@gmail.com>
2020-02-08 20:19:01 +0200

    Форматирование кода, изменены классы FontAwesome

--- application/config/pagination.php
+++ application/config/pagination.php
@@ -1,20 +1,20 @@
 <?php
-$config['first_link'] = '<i class="fa-angle-double-left"></i>';
-$config['next_link'] = '<i class="fa-angle-right"></i>';
-$config['prev_link'] = '<i class="fa-angle-left"></i>';
-$config['last_link'] = '<i class="fa-angle-double-right"></i>';
-$config['cur_tag_open'] = '<li class="pag_active"><a href="#">';//текущую страницу
-$config['cur_tag_close'] = '</a></li>';//текущую страницу
-$config['num_tag_open'] = '<li>';//номера страниц
-$config['num_tag_close'] = '</li>';//номера страниц
-$config['next_tag_open'] = '<li>';//в право
-$config['next_tag_close'] = '</li>';//в право
-$config['prev_tag_open'] = '<li>';//в лево
-$config['prev_tag_close'] = '</li>';//в лево
-$config['first_tag_open'] = '<li>';//в начало
-$config['first_tag_close'] = '</li>';//в начало
-$config['last_tag_open'] = '<li>';//в конец
-$config['last_tag_close'] = '</li>';//
-$config['full_tag_open'] = '<div class="pagination"><ul>';//обернуть всю нумерацию страниц
-$config['full_tag_close'] = '</ul></div>';//обернуть всю нумерацию страниц
-$config['anchor_class'] = '';//
+$config['first_link'] = '<i class="fas fa-angle-double-left"></i>';
+$config['next_link'] = '<i class="fas fa-angle-right"></i>';
+$config['prev_link'] = '<i class="fas fa-angle-left"></i>';
+$config['last_link'] = '<i class="fas fa-angle-double-right"></i>';
+$config['cur_tag_open'] = '<li class="pag_active"><span>'; // текущую страницу
+$config['cur_tag_close'] = '</span></li>'; // текущую страницу
+$config['num_tag_open'] = '<li>'; // номера страниц
+$config['num_tag_close'] = '</li>'; // номера страниц
+$config['next_tag_open'] = '<li>'; // в право
+$config['next_tag_close'] = '</li>'; // в право
+$config['prev_tag_open'] = '<li>'; // в лево
+$config['prev_tag_close'] = '</li>'; // в лево
+$config['first_tag_open'] = '<li>'; // в начало
+$config['first_tag_close'] = '</li>'; // в начало
+$config['last_tag_open'] = '<li>'; // в конец
+$config['last_tag_close'] = '</li>'; //
+$config['full_tag_open'] = '<div class="pagination"><ul>'; // обернуть всю нумерацию страниц
+$config['full_tag_close'] = '</ul></div>'; // обернуть всю нумерацию страниц
+$config['anchor_class'] = ''; //
