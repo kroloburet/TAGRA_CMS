d91262a1d84f97f00ed4e04100ce361096b3b935
kroloburet <kroloburet@gmail.com>
2020-02-08 20:16:21 +0200

    Форматирование кода

--- application/views/404_view.php
+++ application/views/404_view.php
@@ -1,11 +1,15 @@
-<!--####### Mine #######-->
-<div class="mine_wrapper">
- <div class="container" style="max-width:<?=htmlspecialchars($conf['body_width'])?>px">
-  <div class="div_404">
-   <img src="/img/404.jpg" class="img_404">
-   <div class="content_404">
-    <?=$lexic['404']['content']?>
-   </div>
-  </div>
- </div>
-</div>
+
+    <!--
+    ########### Mine
+    -->
+
+    <div class="mine_wrapper">
+      <div class="container">
+        <div class="div_404">
+          <img src="/img/404.jpg" class="img_404">
+          <div class="content_404">
+            <?= $lexic['404']['content'] ?>
+          </div>
+        </div>
+      </div>
+    </div>
