ed62af23c71341b1548d8b0d66148a0c6308c736
kroloburet <kroloburet@gmail.com>
2020-04-26 22:49:15 +0300

    Формат кода

--- application/views/404_view.php
+++ application/views/404_view.php
@@ -1,15 +1,12 @@
+<!--
+########### Main
+-->
 
-    <!--
-    ########### Mine
-    -->
-
-    <div class="mine_wrapper">
-      <div class="container">
-        <div class="div_404">
-          <img src="/img/404.jpg" class="img_404">
-          <div class="content_404">
+<main>
+    <div class="container TUI_align-c">
+        <img src="/img/404.jpg" class="img_404" alt="Error 404">
+        <div class="content_404">
             <?= $lexic['404']['content'] ?>
-          </div>
         </div>
-      </div>
     </div>
+</main>
