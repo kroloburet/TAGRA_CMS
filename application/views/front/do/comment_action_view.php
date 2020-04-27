b4acf3657255918b612f2a8cb6d80c6b92dcfb4c
kroloburet <kroloburet@gmail.com>
2020-04-27 11:25:39 +0300

    Формат кода, доработки
      - Добавление префикса TUI_ классам UI
      - Для fontawesome используется kit.fontawesome.com

--- application/views/front/do/comment_action_view.php
+++ application/views/front/do/comment_action_view.php
@@ -1,25 +1,45 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta name="generator" content="Powered by Tagra CMS. Development and design by Sergey Nizhnik kroloburet@gmail.com">
+<head>
+    <meta name="generator"
+          content="Powered by Tagra CMS. Development and design by Sergey Nizhnik kroloburet@gmail.com">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="none">
-    <link href="/Tagra_UI/fonts/FontAwesome/css/all.min.css" rel="stylesheet">
     <link href="/Tagra_UI/style.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/bacee63d78.js" defer></script>
     <title>Действия над комментарием</title>
     <style>
-      .comment_action_box{display:block;width:50%;margin:15% auto 11% auto;}
-      @media(max-width:1000px){.comment_action_box{width:80%;}}
-      @media(max-width:800px){.comment_action_box{width:90%;}}
-      @media(max-width:600px){.comment_action_box{width:98%;}}
+        .comment_action_box {
+            display: block;
+            width: 50%;
+            margin: 15% auto 11% auto;
+        }
+
+        @media (max-width: 1000px) {
+            .comment_action_box {
+                width: 80%;
+            }
+        }
+
+        @media (max-width: 800px) {
+            .comment_action_box {
+                width: 90%;
+            }
+        }
+
+        @media (max-width: 600px) {
+            .comment_action_box {
+                width: 98%;
+            }
+        }
     </style>
-  </head>
-  <body>
-    <div class="comment_action_box <?= $msg_class ?>"><?= $msg ?></div>
-    <script>
-      window.setTimeout(window.close, 5000);
-    </script>
-  </body>
+</head>
+<body>
+<div class="comment_action_box <?= $msg_class ?>"><?= $msg ?></div>
+<script>
+    window.setTimeout(window.close, 5000);
+</script>
+</body>
 </html>
