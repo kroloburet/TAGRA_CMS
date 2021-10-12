9bb83d5ac0b4ca12526f5028ac87409e23bfed0c
kroloburet <kroloburet@gmail.com>
2021-10-12 10:02:20 +0300

    Изменить название: переменных, таблицы БД, шаблонов. gallerys > galleries;

--- application/models/Back_gallery_model.php
+++ application/models/Back_gallery_model.php
@@ -24,9 +24,9 @@ class Back_gallery_model extends Back_basic_model
      */
     function add_gallery(array $data)
     {
-        $res = $this->db->insert('gallerys', $data);
+        $res = $this->db->insert('galleries', $data);
         // добавить связи с материалом в версиях
-        $res && $data['versions'] ? $this->set_versions('gallerys', $data) : null;
+        $res && $data['versions'] ? $this->set_versions('galleries', $data) : null;
         return $res;
     }
 
@@ -39,11 +39,11 @@ class Back_gallery_model extends Back_basic_model
      */
     function edit_gallery(string $id, array $data)
     {
-        $q = $this->db->where('id', $id)->get('gallerys')->result_array(); // изменяемый материал
+        $q = $this->db->where('id', $id)->get('galleries')->result_array(); // изменяемый материал
         if ($q[0]['versions'] !== $data['versions']) {// версии изменились
-            $this->set_versions('gallerys', $data, $q[0]); // добавить/обновить связи с материалом в версиях
+            $this->set_versions('galleries', $data, $q[0]); // добавить/обновить связи с материалом в версиях
         }
-        return $this->db->update('gallerys', $data, ['id' => $id]);
+        return $this->db->update('galleries', $data, ['id' => $id]);
     }
 
     /**
@@ -59,13 +59,13 @@ class Back_gallery_model extends Back_basic_model
         $url = 'gallery/' . $id;
         // удалить материал и комментарии
         if (
-            $this->db->delete('gallerys', ['id' => $id]) === false ||
+            $this->db->delete('galleries', ['id' => $id]) === false ||
             $this->db->delete('comments', ['url' => $url]) === false
         ) {
             return false;
         }
         // удалить связи с материалом в версиях
-        $this->del_versions('gallerys', '/' . $url);
+        $this->del_versions('galleries', '/' . $url);
         return true;
     }
 }
