e46461bc60c8ecdfb543c0f983121af41cbb796f
kroloburet <kroloburet@gmail.com>
2021-10-12 12:38:43 +0300

    Изменить таблицы и поля БД
    - Изменить тип date > datetime полей: creation_date, last_mod_date
    - Изменить config.layout_l_width > config.content_l_width
    - Изменить имена полей: layout_t > content_t; layout_l > content_l; layout_r > content_r; layout_b > content_b; layout_l_width > content_l_width
    - Изменить тип varchar(250) > bigint поля id

--- instal/process.php
+++ instal/process.php
@@ -23,8 +23,7 @@
      */
 
     $_POST = array_map('trim', $_POST); // убирать пробелы в начале и конце
-    $date = date('Y-m-d'); // текущая дата
-    $moment = date('Y-m-d H:i:s'); // текущая дата и время
+    $datetime = date('Y-m-d H:i:s'); // текущая дата и время
     $ip = $_SERVER["REMOTE_ADDR"]; // текущий IP
     $id = round(microtime(true) * 1000); // уникальный id
 
@@ -37,27 +36,22 @@
 
     // начальные установки системы
     $site_name = $_POST['site_name'];
-    $domen = $_POST['domen'];
+    $domain = $_POST['domain'];
     $admin_mail = $_POST['admin_mail'];
-    $moder_mail = $_POST['moder_mail'];
 
     // шифрование логина\пароля
     // админ
     $admin_name = password_hash($_POST['admin_name'], PASSWORD_BCRYPT);
     $admin_pass = password_hash($_POST['admin_pass'], PASSWORD_BCRYPT);
 
-    // модератор
-    $moder_name = password_hash($_POST['moder_name'], PASSWORD_BCRYPT);
-    $moder_pass = password_hash($_POST['moder_pass'], PASSWORD_BCRYPT);
-
     // сообщения пользователю о статусе установки
-    $good_msg = '<h2 style="margin-top:1.5em">Отлично, вы установили систему!</h2>Инсталлятор сделал свою работу и теперь, для безопасности системы, его необходимо удалить. Нажмите на <q>Удалить инсталлятор</q> или удалите вручную папку <b>/instal</b> со всем ее содержимым.<p>Да, вот еще что! Часто бывает, что систему нужно расширять, модифицировать под ваши персональные нужды для повышения эффективности и конверсии сайта. Поэтому, и в целях скромной саморекламы, я, как разработчик системы, внизу страниц сайта оставил свой email, по которому со мной всегда можно связаться &mdash; пожалуйта, не удаляйте его. Приятного использования!</p><div class="TUI_fieldset"><a href="/instal/destructor.php" class="TUI_btn-link">Удалить инсталлятор</a></div>';
-    $bad_msg = '<h2 style="margin-top:1.5em">Упс! Что-то пошло не так..(</h2><p>Возможно вы допустили ошибку при заполнении полей с данными доступа к базе или недостаточно прав доступа к файлу или папке, а может это какие-то проблемы с сервером. Вы можете вернуться назад и повторить попытку или обратиться к разработчику по e-mail: <a href="mailto:kroloburet@gmail.com">kroloburet@gmail.com</a></p><div class="TUI_fieldset"><a href="#" class="TUI_btn-link" onclick="window.history.back();return false">Вернуться назад</a></div>';
+    $good_msg = '<h2 style="margin-top:1.5em">Отлично, вы установили систему!</h2>Инсталлятор сделал свою работу и теперь, для безопасности системы, его необходимо удалить. Нажмите на <q>Удалить инсталлятор</q> или удалите вручную каталог <b>/instal</b> со всем ее содержимым.<p>Да, вот еще что! Часто бывает, что систему нужно расширять, модифицировать под ваши персональные нужды для повышения эффективности и конверсии сайта. Поэтому, и в целях скромной саморекламы, я, как разработчик системы, внизу страниц сайта оставил свой email, по которому со мной всегда можно связаться &mdash; пожалуйста, не удаляйте его. Приятного использования!</p><div class="TUI_fieldset"><a href="/instal/destructor.php" class="TUI_btn-link">Удалить инсталлятор</a></div>';
+    $bad_msg = '<h2 style="margin-top:1.5em">Упс! Что-то пошло не так..(</h2><p>Возможно вы допустили ошибку при заполнении полей с данными доступа к базе или недостаточно прав доступа к файлу или каталогу, а может это какие-то проблемы с сервером. Вы можете вернуться назад и повторить попытку или обратиться к разработчику по e-mail: <a href="mailto:kroloburet@gmail.com">kroloburet@gmail.com</a></p><div class="TUI_fieldset"><a href="#" class="TUI_btn-link" onclick="window.history.back();return false">Вернуться назад</a></div>';
 
     // промо-данные для записи в БД
     $index_title = 'Привет, Мир!';
-    $index_layout_l = '<p><img src="/img/tagra_share.svg" alt="Tagra CMS"></p>';
-    $index_layout_r = '<p>Добро пожаловать в систему управления контентом <q>Tagra</q>!<br> Итак. Для быстрого старта вашего сайта &mdash; <a href="/admin">зайдите в админку</a>, используя логин и пароль, созданный вами при установке системы, и начинайте творить..) Но прежде, чтобы облегчить работу с самой системой и верстку контента для вашего сайта, я предлагаю <a href="/Tagra_UI/info.html" target="_blank">краткое знакомство с системой</a></p>';
+    $index_content_l = '<p><img src="/img/tagra_share.svg" alt="Tagra CMS"></p>';
+    $index_content_r = '<p>Добро пожаловать в систему управления контентом <q>Tagra</q>!<br> Итак. Для быстрого старта вашего сайта &mdash; <a href="/admin">зайдите в админку</a>, используя логин и пароль, созданный вами при установке системы, и начинайте творить..) Но прежде, чтобы облегчить работу с самой системой и верстку контента для вашего сайта, я предлагаю <a href="/Tagra_UI/info.html" target="_blank">краткое знакомство с системой</a></p>';
     $contact_title = 'Контакты';
 
     /**
@@ -89,8 +83,8 @@ ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;")
     $t = $db_tabl_prefix . 'languages';
     $q_c = $db->query("CREATE TABLE IF NOT EXISTS `$t`(
 `id` bigint UNSIGNED NOT NULL,
-`creation_date` date NOT NULL,
-`last_mod_date` date NOT NULL,
+`creation_date` datetime NOT NULL,
+`last_mod_date` datetime NOT NULL,
 `tag` varchar(10) NOT NULL,
 `title` varchar(20) NOT NULL,
 `def` tinyint(1) UNSIGNED NOT NULL DEFAULT 0,
@@ -98,7 +92,7 @@ ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;")
 PRIMARY KEY (`id`,`tag`))
 ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;");
     $q_i = $db->query("INSERT INTO `$t` (`id`,`creation_date`,`last_mod_date`,`tag`,`title`,`def`) VALUES
-($id,$date,'$date','ru','RU',1);");
+($id,'$datetime','$datetime','ru','RU',1);");
     if (!$q_c) {
         die("<div class='TUI_notice-r'>Не удалось создать таблицу <q>$t</q>: $db->error</div>$bad_msg");
     }
@@ -119,13 +113,13 @@ ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;");
 ('site_access','1'),
 ('site_name','$site_name'),
 ('site_mail','$admin_mail'),
-('jq','https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js'),
+('jq','https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js'),
 ('emmet','0'),
 ('gapi_key',''),
 ('body_width','1000'),
-('layout_l_width','60'),
+('content_l_width','60'),
 ('addthis','{\"js\":\"\",\"share\":\"\",\"follow\":\"\",\"share_def\":\"1\",\"follow_def\":\"1\"}'),
-('img_prev_def','{$domen}img/tagra_share.jpg'),
+('img_prev_def','{$domain}img/tagra_share.jpg'),
 ('breadcrumbs','{\"public\":\"1\",\"home\":\"1\"}'),
 ('markup_data','1'),
 ('sitemap','{\"generate\":\"auto\",\"allowed\":\"public\"}'),
@@ -155,8 +149,7 @@ ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;");
 PRIMARY KEY (`id`))
 ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;");
     $q_i = $db->query("INSERT INTO `$t` (`creation_date`,`last_mod_date`,`last_login_date`,`ip`,`status`,`login`,`password`,`email`) VALUES
-('$moment','$moment','$moment','$ip','administrator','$admin_name','$admin_pass','$admin_mail'),
-('$moment','$moment','$moment','$ip','moderator','$moder_name','$moder_pass','$moder_mail');");
+('$datetime','$datetime','$datetime','$ip','administrator','$admin_name','$admin_pass','$admin_mail');");
     if (!$q_c) {
         die("<div class='TUI_notice-r'>Не удалось создать таблицу <q>$t</q>: $db->error</div>$bad_msg");
     }
@@ -170,26 +163,26 @@ ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;");
     $t = $db_tabl_prefix . 'index_pages';
     $q_c = $db->query("CREATE TABLE IF NOT EXISTS `$t`(
 `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
-`creation_date` date NOT NULL,
-`last_mod_date` date NOT NULL,
+`creation_date` datetime NOT NULL,
+`last_mod_date` datetime NOT NULL,
 `title` varchar(250) NOT NULL,
 `description` varchar(250) NOT NULL,
 `robots` varchar(45) NOT NULL DEFAULT 'all',
 `css` text NOT NULL,
 `js` text NOT NULL,
-`layout_t` text NOT NULL,
-`layout_l` text NOT NULL,
-`layout_r` text NOT NULL,
-`layout_b` text NOT NULL,
-`layout_l_width` int(4) UNSIGNED NOT NULL,
+`content_t` text NOT NULL,
+`content_l` text NOT NULL,
+`content_r` text NOT NULL,
+`content_b` text NOT NULL,
+`content_l_width` int(4) UNSIGNED NOT NULL,
 `addthis_share` tinyint(1) UNSIGNED NOT NULL DEFAULT 1,
 `addthis_follow` tinyint(1) UNSIGNED NOT NULL DEFAULT 1,
 `img_prev` text NOT NULL,
 `lang` varchar(10) NOT NULL,
 PRIMARY KEY (`id`))
 ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;");
-    $q_i = $db->query("INSERT INTO `$t` (`creation_date`,`last_mod_date`,`title`,`description`,`css`,`js`,`layout_t`,`layout_l`,`layout_r`,`layout_b`,`layout_l_width`,`img_prev`,`lang`) VALUES
-('$date','$date','$index_title','$index_title','','','','$index_layout_l','$index_layout_r','',25,'{$domen}img/tagra_share.jpg','ru');");
+    $q_i = $db->query("INSERT INTO `$t` (`creation_date`,`last_mod_date`,`title`,`description`,`css`,`js`,`content_t`,`content_l`,`content_r`,`content_b`,`content_l_width`,`img_prev`,`lang`) VALUES
+('$datetime','$datetime','$index_title','$index_title','','','','$index_content_l','$index_content_r','',25,'{$domain}img/tagra_share.jpg','ru');");
     if (!$q_c) {
         die("<div class='TUI_notice-r'>Не удалось создать таблицу <q>$t</q>: $db->error</div>$bad_msg");
     }
@@ -203,18 +196,18 @@ ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;");
     $t = $db_tabl_prefix . 'contact_pages';
     $q_c = $db->query("CREATE TABLE IF NOT EXISTS `$t`(
 `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
-`creation_date` date NOT NULL,
-`last_mod_date` date NOT NULL,
+`creation_date` datetime NOT NULL,
+`last_mod_date` datetime NOT NULL,
 `title` varchar(250) NOT NULL,
 `description` varchar(250) NOT NULL,
 `robots` varchar(45) NOT NULL DEFAULT 'all',
 `css` text NOT NULL,
 `js` text NOT NULL,
-`layout_t` text NOT NULL,
-`layout_l` text NOT NULL,
-`layout_r` text NOT NULL,
-`layout_b` text NOT NULL,
-`layout_l_width` int(4) UNSIGNED NOT NULL,
+`content_t` text NOT NULL,
+`content_l` text NOT NULL,
+`content_r` text NOT NULL,
+`content_b` text NOT NULL,
+`content_l_width` int(4) UNSIGNED NOT NULL,
 `contacts` text NOT NULL,
 `addthis_share` tinyint(1) UNSIGNED NOT NULL DEFAULT 1,
 `addthis_follow` tinyint(1) UNSIGNED NOT NULL DEFAULT 1,
@@ -223,8 +216,8 @@ ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;");
 `lang` varchar(10) NOT NULL,
 PRIMARY KEY (`id`))
 ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;");
-    $q_i = $db->query("INSERT INTO `$t` (`creation_date`,`last_mod_date`,`title`,`description`,`css`,`js`,`layout_t`,`layout_l`,`layout_r`,`layout_b`,`layout_l_width`,`contacts`,`img_prev`,`lang`) VALUES
-('$date','$date','$contact_title','$contact_title','','','','','','',60,'','{$domen}img/tagra_share.jpg','ru');");
+    $q_i = $db->query("INSERT INTO `$t` (`creation_date`,`last_mod_date`,`title`,`description`,`css`,`js`,`content_t`,`content_l`,`content_r`,`content_b`,`content_l_width`,`contacts`,`img_prev`,`lang`) VALUES
+('$datetime','$datetime','$contact_title','$contact_title','','','','','','',60,'','{$domain}img/tagra_share.jpg','ru');");
     if (!$q_c) {
         die("<div class='TUI_notice-r'>Не удалось создать таблицу <q>$t</q>: $db->error</div>$bad_msg");
     }
@@ -237,19 +230,19 @@ ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;");
     // pages
     $t = $db_tabl_prefix . 'pages';
     $db->query("CREATE TABLE IF NOT EXISTS `$t`(
-`id` bigint UNSIGNED NOT NULL,
-`creation_date` date NOT NULL,
-`last_mod_date` date NOT NULL,
+`id` varchar(250) NOT NULL,
+`creation_date` datetime NOT NULL,
+`last_mod_date` datetime NOT NULL,
 `title` varchar(250) NOT NULL,
 `description` varchar(250) NOT NULL,
 `robots` varchar(45) NOT NULL DEFAULT 'all',
 `css` text NOT NULL,
 `js` text NOT NULL,
-`layout_t` text NOT NULL,
-`layout_l` text NOT NULL,
-`layout_r` text NOT NULL,
-`layout_b` text NOT NULL,
-`layout_l_width` int(4) UNSIGNED NOT NULL,
+`content_t` text NOT NULL,
+`content_l` text NOT NULL,
+`content_r` text NOT NULL,
+`content_b` text NOT NULL,
+`content_l_width` int(4) UNSIGNED NOT NULL,
 `section` varchar(250) NOT NULL,
 `addthis_share` tinyint(1) UNSIGNED NOT NULL DEFAULT 1,
 `addthis_follow` tinyint(1) UNSIGNED NOT NULL DEFAULT 1,
@@ -263,22 +256,22 @@ ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;")
     or die("<div class='TUI_notice-r'>Не удалось создать таблицу <q>$t</q>: $db->error</div>$bad_msg");
     echo "<div class='TUI_notice-g'>Таблица <q>$t</q> успешно создана</div>";
 
-    // gallerys
-    $t = $db_tabl_prefix . 'gallerys';
+    // galleries
+    $t = $db_tabl_prefix . 'galleries';
     $db->query("CREATE TABLE IF NOT EXISTS `$t`(
-`id` bigint UNSIGNED NOT NULL,
-`creation_date` date NOT NULL,
-`last_mod_date` date NOT NULL,
+`id` varchar(250) NOT NULL,
+`creation_date` datetime NOT NULL,
+`last_mod_date` datetime NOT NULL,
 `title` varchar(250) NOT NULL,
 `description` varchar(250) NOT NULL,
 `robots` varchar(45) NOT NULL DEFAULT 'all',
 `css` text NOT NULL,
 `js` text NOT NULL,
-`layout_t` text NOT NULL,
-`layout_b` text NOT NULL,
-`layout_l` text NOT NULL,
-`layout_r` text NOT NULL,
-`layout_l_width` int(4) UNSIGNED NOT NULL,
+`content_t` text NOT NULL,
+`content_b` text NOT NULL,
+`content_l` text NOT NULL,
+`content_r` text NOT NULL,
+`content_l_width` int(4) UNSIGNED NOT NULL,
 `section` varchar(250) NOT NULL,
 `gallery_type` varchar(20) NOT NULL,
 `gallery_opt` text NOT NULL,
@@ -297,19 +290,19 @@ ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;")
     // sections
     $t = $db_tabl_prefix . 'sections';
     $db->query("CREATE TABLE IF NOT EXISTS `$t`(
-`id` bigint UNSIGNED NOT NULL,
-`creation_date` date NOT NULL,
-`last_mod_date` date NOT NULL,
+`id` varchar(250) NOT NULL,
+`creation_date` datetime NOT NULL,
+`last_mod_date` datetime NOT NULL,
 `title` varchar(250) NOT NULL,
 `description` varchar(250) NOT NULL,
 `robots` varchar(45) NOT NULL DEFAULT 'all',
 `css` text NOT NULL,
 `js` text NOT NULL,
-`layout_t` text NOT NULL,
-`layout_b` text NOT NULL,
-`layout_l` text NOT NULL,
-`layout_r` text NOT NULL,
-`layout_l_width` int(4) UNSIGNED NOT NULL,
+`content_t` text NOT NULL,
+`content_b` text NOT NULL,
+`content_l` text NOT NULL,
+`content_r` text NOT NULL,
+`content_l_width` int(4) UNSIGNED NOT NULL,
 `section` varchar(250) NOT NULL,
 `addthis_share` tinyint(1) UNSIGNED NOT NULL DEFAULT 1,
 `addthis_follow` tinyint(1) UNSIGNED NOT NULL DEFAULT 1,
@@ -347,7 +340,7 @@ ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;")
 `premod_code` text NOT NULL,
 `ip` varchar(45) NOT NULL,
 `url` text NOT NULL,
-`creation_date` date NOT NULL,
+`creation_date` datetime NOT NULL,
 `name` varchar(100) NOT NULL,
 `comment` text NOT NULL,
 `rating` text NOT NULL,
@@ -372,8 +365,8 @@ ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;")
     $conf = '<?php defined("BASEPATH") OR exit("No direct script access allowed");' . PHP_EOL
         . '$config["app"] = [];' . PHP_EOL
         . '$config["tagra_version"] = "4.5";' . PHP_EOL
-        . '$config["tagra_instal_date"] = "' . $moment . '";' . PHP_EOL
-        . '$config["base_url"] = "' . $domen . '";' . PHP_EOL
+        . '$config["tagra_instal_date"] = "' . $datetime . '";' . PHP_EOL
+        . '$config["base_url"] = "' . $domain . '";' . PHP_EOL
         . '$config["index_page"] = "";' . PHP_EOL
         . '$config["uri_protocol"] = "REQUEST_URI";' . PHP_EOL
         . '$config["url_suffix"] = "";' . PHP_EOL
@@ -451,12 +444,12 @@ ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;")
         . '<!-- Generator: Tagra CMS -->' . PHP_EOL
         . '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . PHP_EOL
         . '<!-- Home page -->' . PHP_EOL
-        . '<url><loc>' . $domen . '</loc></url>' . PHP_EOL
+        . '<url><loc>' . $domain . '</loc></url>' . PHP_EOL
         . '<!-- Contact page -->' . PHP_EOL
-        . '<url><loc>' . $domen . 'contact</loc></url>' . PHP_EOL
+        . '<url><loc>' . $domain . 'contact</loc></url>' . PHP_EOL
         . '<!-- Pages -->' . PHP_EOL
         . '<!-- Sections -->' . PHP_EOL
-        . '<!-- Gallerys -->' . PHP_EOL
+        . '<!-- Galleries -->' . PHP_EOL
         . '</urlset>';
 
     // каталог для загрузки медиа
